5fcfb537668e33c3d70d1319dc37b423bebd39f7
EC2 Default User <ec2-user@ip-172-31-51-227.ec2.internal>
2016-09-20 22:14:36 +0000

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'web'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures web'
+long_description 'Installs/Configures web'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/web/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/web' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: web
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
